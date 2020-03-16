3ddf3ef641762bba30c07a992de35da0772aa667
faster13 <damashkevichv@gmail.com>
2020-03-16 20:37:36 +0300

    Configured

--- src/test/java/org/sample/args/TestArg.java
+++ src/test/java/org/sample/args/TestArg.java
@@ -1,16 +1,22 @@
 package org.sample.args;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.junit4.DisplayName;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class TestArg {
 
     @Test
+    @DisplayName("Всегда успешно")
+    @Description("Пробный тест, который выполняется без ошибок")
     public void test() {
         System.out.println("Test Done!");
     }
 
     @Test
+    @DisplayName("Всегда провальный")
+    @Description("Пробный тест, который всегда выполняется с ошибкой")
     public void failureTest() {
         Assert.assertTrue(1 == 2);
     }
