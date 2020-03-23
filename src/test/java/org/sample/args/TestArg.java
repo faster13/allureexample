a0d9b57cd9a0bbbfba9feea05cfdc8680ebb85b5
faster13 <damashkevichv@gmail.com>
2020-03-23 22:48:04 +0300

    Parameters

--- src/test/java/org/sample/args/TestArg.java
+++ src/test/java/org/sample/args/TestArg.java
@@ -2,27 +2,45 @@ package org.sample.args;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.Step;
+import io.qameta.allure.Story;
 import io.qameta.allure.junit4.DisplayName;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
 public class TestArg {
 
-    private String something = "YES";
+    @Parameterized.Parameter
+    public String something;
 
-    @Test
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+           {"YES"},
+           {"NO"}
+        });
+    }
+
+
+/*    @Test
     @DisplayName("Всегда успешно")
     @Description("Пробный тест, который выполняется без ошибок")
     public void test() {
         tt();
-    }
+    }*/
 
-    @Step("Only once")
+/*    @Step("Only once")
     public void tt() {
         Assert.assertTrue(1 == 1);
-    }
+    }*/
 
     @Test
+    @Story("Good story")
     @DisplayName("Всегда провальный")
     @Description("Пробный тест, который всегда выполняется с ошибкой")
     public void failureTest() {
